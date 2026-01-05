831b942b8b8a0b59c89225d48a9e23f8258891a3
sohil-khann <khaa40408@gmail.com>
2026-01-05 12:14:29 +0530

    [sohil khan] Add. polymorphism achieved,  Use case 05

--- Eco-Ride/ElectricScooter.cs
+++ Eco-Ride/ElectricScooter.cs
@@ -2,7 +2,7 @@
 using System;
 public class  ElectricScooter : Vehicle
 {
-
+    //private field
     int max_speed_limit;
     public ElectricScooter(string vehicle_id, string model, int speed_limit) : base(vehicle_id, model)
     {
@@ -12,10 +12,13 @@ public class  ElectricScooter : Vehicle
     {
         return max_speed_limit;
     }
+    //overriding the abstract method from Vehicle class
     public override int calculate_trip_cost(int distance)
     {
-        int cost_per_km = GetRentalPrice();
-        return cost_per_km * distance;
+        
+        int base_rate = 1; // Base rate per kilometer
+        double cost_per_min=0.15; // Cost per minute
+        return (base_rate+cost_per_min)* distance;
     }
 
 }
