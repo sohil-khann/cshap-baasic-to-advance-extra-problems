273d8ab358e406055e996ae3b49f614f8f5a9e98
sohil-khann <khaa40408@gmail.com>
2026-01-05 11:46:57 +0530

    [sohil khan] Add. Use case 02

--- Eco-Ride/Vehicle.cs
+++ Eco-Ride/Vehicle.cs
@@ -1,16 +1,49 @@
 using System;
 public class Vehicle
 {
-    public string Vehicle_id { get; set; }
-    public string Model { get; set; }
-    public byte Battery_percentage { get; set; }
-    public Vehicle(string vehicle_id, string model, int battery_percentage)
+    private string Vehicle_id { get; set; }
+    private string Model { get; set; }
+    private byte Battery_percentage { get; set; }
+    private bool Maintenance_Status { get; set; }
+    private int Rental_Price { get; set; }
+    public Vehicle(string vehicle_id, string model)
     {
         Vehicle_id = vehicle_id;
         Model = model;
-        Battery_percentage = battery_percentage;
-       
+
+        
+
     }
+    public bool GetMaintenanceStatus()
+    {
+        return Maintenance_Status;
+    }
+    public void SetMaintenanceStatus(bool status)
+    {
+        Maintenance_Status = status;
+    }
+    public int GetRentalPrice()
+    {
+        return Rental_Price;
+    }
+    public void SetRentalPrice(int price)
+    {
+
+        Rental_Price = price;
+    }
+    public void SetBatteryPercentage(byte percentage)
+    {
+        if(percentage <=100 && percentage>=0)
+        {
+            Battery_percentage = percentage;
+
+        }
+        else
+        {
+            Console.WriteLine("Invalid Battery Percentage");
+        }
+    }
+
     //public override string ToString()
     //{
     //    return $"";
